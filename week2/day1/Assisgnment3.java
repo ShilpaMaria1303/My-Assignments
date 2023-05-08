b61d2b5e8a148bc98609e21b672d6941fe350bef
ShilpaMaria1303 <132162353+ShilpaMaria1303@users.noreply.github.com>
2023-05-08 22:48:08 +0530

    Add files via upload

---
+++ week2/day1/Assisgnment3.java
@@ -0,0 +1,41 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Assisgnment3 {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.findElement(By.id("username")).sendKeys("demoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.partialLinkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Shilpa");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Jose");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Jose");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Automation");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("Learn automation");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("Test@gmail.com");
+		WebElement ss= driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select sec=new Select(ss);
+		sec.selectByVisibleText("Alaska");
+		driver.findElement(By.className("smallSubmit")).click();
+		String s=driver.getTitle();
+		System.out.println(s);
+		Thread.sleep(3000);
+		driver.findElement(By.linkText("Duplicate Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).clear();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf_updated");
+		driver.findElement(By.className("smallSubmit")).click();
+		String s1=driver.getTitle();
+		System.out.println(s1);
+	}
+
+}
